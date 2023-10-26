8c0893d0b72d861bc0536df75bbcedd8edf3ca84
TiagoKblo <kblotiago@gmail.com>
2023-10-25 22:13:23 -0300

    alteração estilo

--- dashboard.php
+++ dashboard.php
@@ -8,6 +8,7 @@ $mensagens = $usuario->buscarMensagens();
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -20,16 +21,14 @@ $mensagens = $usuario->buscarMensagens();
     <link rel="stylesheet" href="estilo.css">
     <title> Mensagens de Contato - Volta ao Mundo</title>
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg" style="background-color: #C8102E;">
         <div class="container">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
                 <span class="navbar-toggler-icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor"
-                        class="bi bi-filter-square-fill" viewBox="0 0 16 16">
-                        <path
-                            d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm.5 5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1 0-1zM4 8.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm2 3a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-filter-square-fill" viewBox="0 0 16 16">
+                        <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm.5 5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1 0-1zM4 8.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm2 3a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5z" />
                     </svg>
                 </span>
             </button>
@@ -60,60 +59,65 @@ $mensagens = $usuario->buscarMensagens();
             </div>
         </div>
     </nav>
-    <main class="painel-mensagens bg-light">
-    <div class="container">
-        <h2 class="text-center mt-4">Mensagens de Contato</h2>
-        <div class="mt-4" id="mensagens-container">
-        <main class="painel-mensagens bg-light">
-            <?php
-            // Loop através das mensagens
-            foreach ($mensagens as $mensagem) {
-                $respondidaClass = ($mensagem['respondida'] == 1) ? 'respondida' : '';
-            ?>
-            <div class="mensagem <?php echo $respondidaClass; ?>">
-                <div class="row">
-                    <div class="col-md-6">
-                        <h4>Remetente: <?= $mensagem['nome'] ?></h4>
-                        <h5>E-mail: <?= $mensagem['email'] ?></h5>
-                        <p>Data: <?= $mensagem['data_envio'] ?></p>
-                    </div>
-                    <div class="col-md-6">
-                        <p>Mensagem: <?= $mensagem['mensagem'] ?></p>
-                    </div>
-                </div>
+    <main class="painel-mensagens">
+        <div class="container">
+            <h2 class="text-center mt-4">Mensagens de Contato</h2>
+            <div class="mt-4" id="mensagens-container">
 
-                <form method="POST" action="processar_resposta.php">
-                    <input type="hidden" name="id_mensagem" value="<?= $mensagem['id'] ?>">
-                    <input type="hidden" name="email_destinatario" value="<?= $mensagem['email'] ?>">
-                    <div class="form-group">
-                        <textarea name="resposta" rows="3" class="form-control" placeholder="Sua resposta"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Responder</button>
-                </form>
+                <?php
+                // Loop através das mensagens
+                foreach ($mensagens as $mensagem) {
+                    $respondidaClass = ($mensagem['respondida'] == 1) ? 'respondida' : '';
+                ?>
+                    <div class="mensagem <?php echo $respondidaClass; ?>">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h4>Remetente: <?= $mensagem['nome'] ?></h4>
+                                <h5>E-mail: <?= $mensagem['email'] ?></h5>
+                                <h5>Data: <?= $mensagem['data_envio'] ?></h5>
+                            </div>
+                            <div class="col-md-6">
+                                <p>Mensagem: <?= $mensagem['mensagem'] ?></p>
+                            </div>
+                        </div>
 
-                <!-- Botão para marcar como respondida -->
-                <?php if ($mensagem['respondida'] == 1) { ?>
-                    <button class="btn btn-secondary respondida">Respondida</button>
-                <?php } else { ?>
-                    <a href="marcar_respondida.php?id=<?= $mensagem['id'] ?>" class="btn btn-success">Marcar como Respondida</a>
-                <?php } ?>
+                        <form method="POST" action="processar_resposta.php">
+                            <input type="hidden" name="id_mensagem" value="<?= $mensagem['id'] ?>">
+                            <input type="hidden" name="email_destinatario" value="<?= $mensagem['email'] ?>">
+                            <div class="form-group">
+    <textarea name="resposta" rows="3" class="form-control" placeholder="Sua resposta">
+        <?php
+        if ($mensagem['respondida'] == 1) {
+            echo isset($mensagem['resposta']) ? $mensagem['resposta'] : '';
+        }
+        ?>
+    </textarea>
+</div>
+
+
+                            <button type="submit" class="btn btn-primary btn-block">Responder</button>
+                        </form>
+
+                        <!-- Botão para marcar como respondida -->
+                        <?php if ($mensagem['respondida'] == 1) { ?>
+                            <button class="btn btn-secondary respondida btn-block">Respondida</button>
+                        <?php } else { ?>
+                            <a href="marcar_respondida.php?id=<?= $mensagem['id'] ?>" class="btn btn-success btn-block">Marcar como Respondida</a>
+                        <?php } ?>
+                    </div>
+                <?php
+                }
+                ?>
             </div>
-            <?php
-            }
-            ?>
         </div>
-    </div>
-</main>
+    </main>
 
     <footer class="text-center">
         <div class="container">
             <p><strong>Projeto Volta ao Mundo - México</strong></p>
             <a href="https://github.com/TiagoKblo"><em>Desenvolvido por Kblo</em></a>
             <br>
-            <audio controls loop>
-                <source src="musica/Latino_Party_-_Sekosh_(2).mp3" type="audio/mp3">
-                Seu navegador não suporta a tag de áudio.
-            </audio>
+
         </div>
     </footer>
 
@@ -121,4 +125,5 @@ $mensagens = $usuario->buscarMensagens();
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
--- processar_resposta.php
+++ processar_resposta.php
@@ -1,27 +1,41 @@
 <?php
+require 'classes/conexao.php'; // Arquivo de conexão com o banco de dados
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Recupere os dados do formulário
     $mensagemId = $_POST['id_mensagem'];
     $resposta = $_POST['resposta'];
 
-    // Aqui você pode adicionar a lógica para salvar a resposta no banco de dados
+    try {
+        // Conectar-se ao banco de dados
+        $conexao = new PDO('mysql:host=127.0.0.1;dbname=voltamundo', 'root', '');
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Configurações de e-mail
-    $to = 'email@destinatario.com';  // Endereço de e-mail do destinatário
-    $subject = 'Resposta à sua mensagem'; // Assunto do e-mail
-    $message = $resposta; // Conteúdo da mensagem
+        // Prepare e execute a consulta SQL para atualizar a resposta na tabela de mensagens
+        $sql = "UPDATE mensagens SET resposta = ? WHERE id = ?";
+        $stmt = $conexao->prepare($sql);
+        $stmt->execute([$resposta, $mensagemId]);
 
-    // Você pode adicionar cabeçalhos adicionais, como "From" ou "Reply-To" conforme necessário
 
-    // Enviar o e-mail
-    if (mail($to, $subject, $message)) {
-        // E-mail enviado com sucesso
-        echo 'Resposta enviada por e-mail com sucesso!';
-    } else {
-        // Erro ao enviar o e-mail
-        echo 'Erro ao enviar a resposta por e-mail.';
+        // Configurações de e-mail
+        $to = 'email@destinatario.com'; // Endereço de e-mail do destinatário
+        $subject = 'Resposta à sua mensagem'; // Assunto do e-mail
+        $message = $resposta; // Conteúdo da mensagem
+
+        // Você pode adicionar cabeçalhos adicionais, como "From" ou "Reply-To" conforme necessário
+
+        // Enviar o e-mail
+        if (mail($to, $subject, $message)) {
+            // E-mail enviado com sucesso
+            echo 'Resposta enviada por e-mail com sucesso!';
+        } else {
+            // Erro ao enviar o e-mail
+            echo 'Erro ao enviar a resposta por e-mail.';
+        }
+
+        // Redirecione de volta ao painel de mensagens
+        header('Location: dashboard.php');
+    } catch (PDOException $e) {
+        // Erro na conexão ou na consulta SQL
+        echo 'Erro: ' . $e->getMessage();
     }
-
-    // Redirecione de volta ao painel de mensagens
-    header('Location: dashboard.php');
 }
