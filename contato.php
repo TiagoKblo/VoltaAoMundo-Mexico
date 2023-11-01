7ea59d20ebc4ad988da349893522085490cb5687
TiagoKblo <kblotiago@gmail.com>
2023-11-01 09:05:42 -0300

    alterações finais de estilo

--- contato.php
+++ contato.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Kblo">
+    <meta name="keywords" content="méxico">
+    <meta name="description" content="Entre em contato conosco">
+    <link rel="icon" href="imagens/icon.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Contato - Volta ao Mundo</title>
+</head>
+
+<body>
 <?php
 require 'classes/conexao.php'; // Arquivo de conexão com o banco de dados
 
@@ -16,12 +33,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmt = $conexao->prepare($sql);
         $stmt->execute([$nome, $email, $mensagem]);
 
-        echo 'Mensagem enviada com sucesso. Entraremos em contato em breve.';
-        //Direcionar para a página anterior
-        echo '<a href="index.html">Voltar</a>';
+         // Mensagem enviada com sucesso
+         echo '<p class="success-message">Mensagem enviada com sucesso. Entraremos em contato em breve.</p>';
+         // Direcionar para a página anterior
+         echo '<a href="index.html">Voltar</a>';
 
-    } catch (PDOException $e) {
-        echo 'Erro no envio da mensagem: ' . $e->getMessage();
-    }
+     } catch (PDOException $e) {
+         // Erro no envio da mensagem
+         echo '<p class="error-message">Erro no envio da mensagem: ' . $e->getMessage() . '</p>';
+     }
 }
 ?>
+</body>
+</html>
