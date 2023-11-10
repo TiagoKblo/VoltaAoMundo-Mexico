5ebdcd0983932fc3a2f89ffb7fa9af8a9f8627a2
TiagoKblo <kblotiago@gmail.com>
2023-11-10 14:27:48 -0300

    ajustando

--- processar_resposta.php
+++ processar_resposta.php
@@ -16,14 +16,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmt->execute([$resposta, $mensagemId]);
 
 
-        // Configurações de e-mail
+        /* Configurações de e-mail
         $to = 'email@destinatario.com'; // Endereço de e-mail do destinatário
         $subject = 'Resposta à sua mensagem'; // Assunto do e-mail
         $message = $resposta; // Conteúdo da mensagem
 
         // Você pode adicionar cabeçalhos adicionais, como "From" ou "Reply-To" conforme necessário
 
-        // Enviar o e-mail
+        /* Enviar o e-mail
         if (mail($to, $subject, $message)) {
             // E-mail enviado com sucesso
             echo 'Resposta enviada por e-mail com sucesso!';
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Erro ao enviar o e-mail
             echo 'Erro ao enviar a resposta por e-mail.';
         }
-
+*/
         // Redirecione de volta ao painel de mensagens
         header('Location: dashboard.php');
     } catch (PDOException $e) {
